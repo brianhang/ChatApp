29feebfb19a38fc54d34612c052982083d6eaff1
Brian Hang <hangwithbrian@gmail.com>
2017-08-08 10:08:13 -0700

    Add Mongoose dependency

--- server/typings/index.d.ts
+++ server/typings/index.d.ts
@@ -2,3 +2,4 @@
 /// <reference path="modules/body-parser/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
 /// <reference path="modules/mongodb/index.d.ts" />
+/// <reference path="modules/mongoose/index.d.ts" />
