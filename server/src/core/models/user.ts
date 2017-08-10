07f6bb42b017dc7a6529b9a310fb28155aba4af8
Brian Hang <hangwithbrian@gmail.com>
2017-08-10 16:54:14 -0700

    Add emit method for User model

--- server/src/core/models/user.ts
+++ server/src/core/models/user.ts
@@ -1,3 +1,16 @@
 export class User {
   public nickname: string;
+  public socket: SocketIO.Socket;
+
+  /**
+   * Emits an event to the socket for this user.
+   * 
+   * @param event The name of the event to emit.
+   * @param data Data about the event.
+   */
+  public emit(event: string, data: any): void {
+    if (this.socket) {
+      this.socket.emit(event, data);
+    }
+  }
 }
--- server/src/core/models/users.ts
+++ server/src/core/models/users.ts
@@ -1,10 +1,17 @@
 import { Schema, model } from 'mongoose';
 import { UserDocument } from '../interfaces/user-document';
+import { User } from './user';
 
 const schema = new Schema({
   nickname: { type: String, required: true }
 });
 
+schema.virtual('socket')
+  .set(function (this: any, socket: SocketIO.Socket): void { this._socket = socket })
+  .get(function (this: any): SocketIO.Socket { return this._socket });
+
+schema.method('emit', User.prototype.emit);
+  
 // Add passport related fields for the users model.
 const passportlocalMongoose = require('passport-local-mongoose');
 schema.plugin(passportlocalMongoose);
