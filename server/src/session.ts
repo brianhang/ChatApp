a5ffc9d2eea6108c9d0ebeeed95de02a5f7aa65d
Brian Hang <hangwithbrian@gmail.com>
2017-08-10 11:52:24 -0700

    Fix cookies expiring after browser closes

--- server/src/session.ts
+++ server/src/session.ts
@@ -4,6 +4,8 @@ const secret = 'VpFNDCJJBFg2MfB5';
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
+const YEAR = 1000 * 60 * 60 * 24 * 30 * 12;
+
 export const sessionConfig = {
   key: key,
   secret: secret,
@@ -11,7 +13,10 @@ export const sessionConfig = {
     url: 'mongodb://localhost/chatDb'
   }),
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  cookie: {
+    maxAge: YEAR
+  }
 };
 
 export const expressSession = session(sessionConfig);
