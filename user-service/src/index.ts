8119fe0718bf5325189c1c6b33c103d3550bcb1a
Brian Hang <hangwithbrian@gmail.com>
2017-08-28 15:27:42 -0700

    Start on services connecting to RabbitMQ

--- user-service/src/index.ts
+++ user-service/src/index.ts
@@ -1,1 +1,16 @@
-console.log('user service 12');
+import * as amqp from 'amqplib/callback_api';
+
+const queue = 'user';
+
+amqp.connect('amqp://rabbitmq', (err, conn) => {
+  if (err) {
+    console.error(err);
+
+    return;
+  }
+
+  conn.createChannel((err, channel) => {
+    channel.assertQueue(queue, { durable: false });
+    channel.sendToQueue(queue, new Buffer('Hello world!'));
+  });
+});
