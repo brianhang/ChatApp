c4bf561605bf25df6283ad839b1ffb66baef4e69
Brian Hang <hangwithbrian@gmail.com>
2017-09-07 09:21:45 -0700

    Move message queue connection to an env variable

--- message-service/src/index.ts
+++ message-service/src/index.ts
@@ -1,6 +1,6 @@
 import { MessageService } from './service';
 import { AmqpGateway } from './gateway/amqp-gateway';
 
-const gateway = new AmqpGateway('amqp://rabbitmq');
+const gateway = new AmqpGateway(process.env.MESSAGE_QUEUE || '');
 const service = new MessageService(gateway);
 service.start();
